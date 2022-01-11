5c7c23785f243ee31c8f75d095b5f0ab8ca8bcc5
mohammed mustafa <mohammed.alkhddar@gmail.com>
2022-01-10 20:53:57 -0600

    added router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import "./App.css";
+import HomePage from "./pages/homepage/HomePage";
+import { Route, Switch } from "react-router";
 
-import './App.css';
-import HomePage from './pages/homepage/HomePage'
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
-    <div >
- <h1>Test</h1>
- <HomePage />
+    <div>
+    <Switch>
+        <Route exact path="/" component={HomePage}/>
+        <Route path="/hatpage" component={HatsPage} />
+        </Switch>
     </div>
   );
 }
